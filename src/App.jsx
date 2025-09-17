fb88fb7b45469d5b1f4370e1de0f1288cf31b229
Nirmal <nirmalmhjr@users.noreply.github.com>
2025-09-17 22:25:04 +0545

    feat(Home) : animation added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <section className="min-h-screen bg-blue-100 z-0"></section>
     </main>
   );
 };
