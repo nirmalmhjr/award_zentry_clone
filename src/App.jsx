46bc1033439bf802ab7a1de65e7aaa741f870ff2
Nirmal <nirmalmhjr@users.noreply.github.com>
2025-09-20 12:48:30 +0545

    page: About section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="min-h-screen bg-blue-100 z-0"></section>
+      {/* <section className="min-h-screen bg-blue-100 z-0"></section> */}
+      <About />
     </main>
   );
 };
